0dfcf719d9e11ba848ca1006ab7b42b1af9eacc0
v_zhangwenzhao <v_zhangwenzhao@bilibili.com>
2019-01-30 18:57:52 +0800

    init:frame

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <div class="demo">
+      1
+    </div>
+  </div>
+</template>
+
+<style lang="less">
+.demo {
+  background-color: pink;
+  border:1px solid #000;
+}
+</style>
